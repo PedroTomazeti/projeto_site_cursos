0a9015ef5f4ccd8082414996fdaf9de6e71f7050
PedroTomazeti <pltf8001@gmail.com>
2024-08-20 12:54:54 -0300

    create: courses model

--- app_site_cursos/models.py
+++ app_site_cursos/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Courses(models.Model):
+
+    choices_hour = (
+        ('30', '30H'),
+        ('45', '45H'),
+        ('60', '60H'),
+        ('90', '90H'),
+        ('120', '120H'),
+    )
+
+    name = models.CharField(max_length = 45, null = False)
+    description = models.TextField(max_length = 300, null = True)
+    start_at = models.DateTimeField(null = False)
+    workload = models.CharField(max_length = 3, choices = choices_hour, null = False)
+
+    def __str__(self) -> str:
+        return self.name
